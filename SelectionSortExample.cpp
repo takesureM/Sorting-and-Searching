480a006e34a91e235ec8481ebe311388222ce838
TakesureM <tiripamwepo@gmail.com>
2020-07-16 17:45:18 +0700

    Update SelectionSortExample.cpp

--- SelectionSortExample.cpp
+++ SelectionSortExample.cpp
@@ -52,27 +52,7 @@ int BinarySearch(int array[], int elements, int value)
 
 }
 
-void BubbleSort(int array[], int size)
-{
-	int temp;
-	bool swap;
 
-	do
-	{
-		swap = false;
-
-		for (int i = 0; i < size - 1; i++)
-		{
-			if (array[i] > array[i + 1])
-			{
-				temp = array[i];
-				array[i] = array[i + 1];
-				array[i + 1] = temp;
-				swap = true;
-			}
-		}
-	} while (swap);
-}
 void selectionSort(int array[], int size)
 {
 	int startScan, minIndex, minValue;
